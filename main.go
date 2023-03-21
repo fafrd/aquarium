a110ad7a5df0cbde8ae0471caac0a19a6e429d61
Kian <fafrd@users.noreply.github.com>
2023-03-21 02:09:45 -0700

    cleanup

--- main.go
+++ main.go
@@ -4,10 +4,7 @@ import (
 	"aquarium/actor"
 	"sync"
 	"time"
-	/*
-		"bufio"
-		"os/exec"
-	*/)
+)
 
 func main() {
 
@@ -34,42 +31,4 @@ func main() {
 	}()
 
 	<-done
-
-	/*
-		cmd := exec.Command("ls", "-l") // Replace with your command
-		stdout, err := cmd.StdoutPipe()
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		stderr, err := cmd.StderrPipe()
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		if err := cmd.Start(); err != nil {
-			fmt.Println(err)
-			return
-		}
-
-		go func() {
-			scanner := bufio.NewScanner(stdout)
-			for scanner.Scan() {
-				fmt.Println(scanner.Text())
-			}
-		}()
-
-		go func() {
-			scanner := bufio.NewScanner(stderr)
-			for scanner.Scan() {
-				fmt.Println(scanner.Text())
-			}
-		}()
-
-		if err := cmd.Wait(); err != nil {
-			fmt.Println(err)
-			return
-		}
-	*/
-
 }
